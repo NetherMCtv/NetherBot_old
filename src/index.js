36373ea9533cbef877c7d22d16171a0129debec3
NetherMC <thelio.trinite@outlook.com>
2021-06-24 13:57:51 +0200

    commandes & events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const fs = require('fs');
 require('dotenv').config();
 
 const client = new Client({ ws: { intents: Intents.ALL } });
+client.commands = new Collection();
 
 client.login(process.env.DISCORD_BOT_TOKEN);

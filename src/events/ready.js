b2e5f2cfbc7b4a51c811e062812f5d7195aec91b
NetherMC <thelio.trinite@outlook.com>
2021-06-24 18:44:48 +0200

    event "ready" + cmd

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,8 +2,22 @@ const Event = require('../helpers/Event');
 
 class ReadyEvent extends Event {
 
-  run(client) {
+  async run(client) {
     console.log(`Connecté en tant que ${client.user.tag}.`);
+
+    const guilds = client.guilds.cache;
+    const statuses = [
+      ['NetherMC streamer', 'STREAMING'],
+      ['des followers', 'WATCHING'],
+    ];
+    let i = 0;
+    setInterval(async () => {
+      await client.user.setActivity(statuses[i][0], {
+        type: statuses[i][1],
+        url: 'https://www.twitch.tv/NetherMCtv'
+      });
+      i = ++i % statuses.length;
+    }, 1e1);
   }
 
 }

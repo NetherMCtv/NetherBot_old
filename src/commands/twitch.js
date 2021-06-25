cad11dd513a9dac21daf8881ce4432217999517b
NetherMC <thelio.trinite@outlook.com>
2021-06-25 11:18:48 +0200

    twitch

---
+++ src/commands/twitch.js
@@ -0,0 +1,41 @@
+const Command = require('../helpers/Command');
+const Embed = require('../helpers/Embed');
+
+class TwitchCommand extends Command {
+
+  constructor() {
+    super();
+
+    this.setHelp({
+      name: 'Twitch',
+      description: 'followers twitch'
+    });
+  }
+
+  async run(message, client) {
+    const embed = new Embed({
+      author: {
+        name: 'NetherMC',
+        url: 'https://www.twitch.tv/NetherMCtv'
+      },
+      title: 'Twitch',
+      description: 'Informations à propos de ma chaîne Twitch.',
+      fields: [
+        {
+          name: 'Followers',
+          value: '<?> followers',
+          inline: true
+        },
+        {
+          name: 'En live',
+          value: 'Non',
+          inline: true
+        },
+      ]
+    }, client);
+    message.channel.send(embed);
+  }
+
+}
+
+module.exports = TwitchCommand;

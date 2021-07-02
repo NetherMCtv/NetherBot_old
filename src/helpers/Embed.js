63a193d950fb53293b730c72bad4cb75f576f22b
NetherMC <thelio.trinite@outlook.com>
2021-07-02 13:39:17 +0200

    embed

--- src/helpers/Embed.js
+++ src/helpers/Embed.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 class Embed {
 
@@ -8,8 +8,10 @@ class Embed {
     let embed;
 
     embed = options;
-    embed.footer = { text: client.user.username, icon_url: client.user.avatarURL() };
-    embed.timestamp = Date.now();
+    (
+      !embed.footer || (!embed.footer.text || !embed.footer.icon_url || !embed.footer.url)
+    ) && (embed.footer = { text: client.user.username, icon_url: client.user.avatarURL() });
+    !embed.timestamp && (embed.timestamp = Date.now());
 
     return new MessageEmbed(embed);
   }

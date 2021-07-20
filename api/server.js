817fa185aa933e9243af9572c2348c0213524080
NetherMC <thelio.trinite@outlook.com>
2021-07-20 17:36:48 +0200

    api

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const server = express();
-const port = 3000;
+const port = 4682;
 
-const req = (method, path, file) => server[method](path, (req, res) => require(file)(req, res));
+const req = (method, path, file) => server[method](path, (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '127.0.0.1:8000');
+  return require(file)(req, res);
+});
 const get = (path, file) => req('get', path, file);
+
+req('all', '/bugs', './bugs/index');
+req('all', '/bugs/:id(\\d+)', './bugs/bug');
 
 get('/bot/config', './bot/config');
 get('/bot/info', './bot/info');
